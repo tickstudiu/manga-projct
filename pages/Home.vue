ac859b38528627bcf80f0b0a1f714850e72eeaad
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-09-11 10:26:17 +0700

    update home page

--- pages/Home.vue
+++ pages/Home.vue
@@ -2,18 +2,19 @@
   <div class="home">
     <!-- search form -->
     <form @submit.prevent="onSubmit" class="flex gap-6">
-      <input type="text" placeholder="search" class="flex-1 border"/>
+      <input type="text" placeholder="search" class="flex-1 border" v-model="keyword"/>
       <button class="border px-3">submit</button>
       <client-only>
-        <select>
-          <option value="value">filter type</option>
+        <select v-model="category">
+          <option value="" disabled>select one</option>
+          <option v-for="(category, index) in Object.values(categories)" :key="index" :value="category">{{category}}</option>
         </select>
       </client-only>
     </form>
 
     <!-- new book -->
     <div class="flex justify-between items-baseline">
-      <h2>new book</h2>
+      <h2>new books</h2>
       <nuxt-link to="/" class="hover:underline"> more </nuxt-link>
     </div>
     <div>
@@ -22,11 +23,11 @@
       <BookCard class="mb-3"/>
     </div>
 
-    <hr />
+    <hr class="my-6"/>
 
     <!-- all chapters order by dec -->
     <div class="flex justify-between items-baseline">
-      <h2>chapters</h2>
+      <h2>last updated chapters</h2>
       <nuxt-link to="/" class="hover:underline"> more </nuxt-link>
     </div>
     <div>
@@ -40,9 +41,18 @@
 <script lang="ts">
 import Vue from 'vue'
 import BookCard from '@/components/books/BookCard.vue'
+import {categories} from '@/enums/categories'
 export default Vue.extend({
   components: {
     BookCard,
+  },
+
+  data(){
+    return {
+      categories,
+      category: '',
+      keyword: '',
+    }
   },
 
   methods: {
