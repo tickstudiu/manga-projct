d581a1e9e1578bb3a18459f5bb337675bae00fe5
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-09-12 14:39:21 +0700

    update image render

---
+++ components/ImageRender.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <img
+      :src="thumbnail.url"
+      :alt="thumbnail.alt"
+      class="border p-3"
+      :class="{ hidden: isLoading }"
+      @load="onImgLoaded"
+    />
+    <div v-if="isLoading" class="border p-3 text-center">loading...</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import { PageItem } from '@/types/chapter'
+export default Vue.extend({
+  props: {
+    page: {
+      type: Object,
+      default: null,
+    } as PropOptions<PageItem>,
+  },
+
+  data() {
+    return {
+      isLoading: true,
+    }
+  },
+
+  computed: {
+    thumbnail(): any {
+      return {
+        url: this.page?.pageUrl,
+        alt: this.page?.alt || 'iamge',
+      }
+    },
+  },
+
+  methods: {
+    onImgLoaded() {
+      this.isLoading = false
+    },
+  },
+})
+</script>
