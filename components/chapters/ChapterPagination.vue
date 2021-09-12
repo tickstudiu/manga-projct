b97b0c41a82f4b9ac15e848bf9fc5df6207646cc
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-09-12 15:00:15 +0700

    update pagination chapter

---
+++ components/chapters/ChapterPagination.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-between">
+    <button
+      class="border px-3"
+      @click="$emit('prev')"
+      @keydown="$emit('prev')"
+      :disabled="disabledNext"
+    >
+      prev
+    </button>
+    <button class="border px-3" @click="$emit('next')" @keydown="$emit('next')">
+      next
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    disabledNext: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
