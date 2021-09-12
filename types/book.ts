c77c80ad404b78b2ccbdb92e3082f6dc866911e5
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-09-12 18:35:31 +0700

    update type

--- types/book.ts
+++ types/book.ts
@@ -1,8 +1,15 @@
+export interface CoverItem {
+    width: number
+    height: number
+    pageUrl: string
+    alt?: string
+}
 export interface BookItem {
     id: number
     categories: number[]
     isNew: boolean
     tags: number[]
+    cover: CoverItem
     authors: number[]
     title: string
     titleSlug: string
