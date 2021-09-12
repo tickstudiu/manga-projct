fffccf40e52228489072804644d79b74f84728ab
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-09-12 17:49:42 +0700

    update loading component

---
+++ components/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="loading">loading...</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      loading: false,
+    }
+  },
+
+  methods: {
+    start() {
+      this.loading = true
+    },
+    finish() {
+      this.loading = false
+    },
+  },
+})
+</script>
