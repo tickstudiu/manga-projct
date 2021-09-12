fd4625f3e69158cddb3c03fc664834c9c8fadb8e
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-09-12 15:55:34 +0700

    update pagiantion chpater

--- pages/Chapter/ChapterDetail.vue
+++ pages/Chapter/ChapterDetail.vue
@@ -1,11 +1,62 @@
 <template>
-  <div class="chapter">
-    <p>chapter id : {{ $route.params.chapter_id }}</p>
+  <div class="chapter-detail">
+    <!-- chapter detail -->
+    <h3 class="mb-3">
+      {{ book.title }}
+    </h3>
+    <p class="mb-3">chapter {{ chapter.ep }} : {{ chapter.name }}</p>
+
+    <!-- image list -->
+    <template v-if="!isLoading">
+      <ImageRender
+        class="mb-3"
+        v-for="(page, index) in chapter.pages"
+        :key="index"
+        :page="page"
+      />
+    </template>
+    <template v-else> loading... </template>
+
+    <!-- pagination -->
+    <ChapterPagination
+      :disabled-next="$route.params.chapter_id === '1'"
+      @next="changeChapter(Number($route.params.chapter_id) + 1)"
+      @prev="changeChapter(Number($route.params.chapter_id) - 1)"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState } from 'vuex'
+import ImageRender from '@/components/ImageRender.vue'
+import ChapterPagination from '@/components/chapters/ChapterPagination.vue'
 export default Vue.extend({
+  components: {
+    ImageRender,
+    ChapterPagination,
+  },
+
+  async fetch() {
+    await this.$store.dispatch('chapterDetail/fetch', {
+      chapterId: this.$route.params.chapter_id,
+      bookId: this.$route.params.book_id,
+    })
+  },
+
+  computed: {
+    ...mapState('chapterDetail', ['isLoading', 'chapter', 'book']),
+  },
+
+  methods: {
+    changeChapter(newChapter: any) {
+      window.location.href = `/books/${this.$route.params.book_id}/${newChapter}`
+    },
+  },
 })
 </script>
+
+<style lang="sass" scoped>
+.chapter-detail
+  @apply py-6 px-4
+</style>
---
+++ store/chapterDetail.ts
@@ -0,0 +1,51 @@
+import { BookItem } from '@/types/book'
+import { ChapterItem } from '@/types/chapter'
+
+export default {
+    state: () => ({
+        isLoading: false,
+        books: {} as BookItem,
+        chapter: {} as ChapterItem,
+    }),
+
+    mutations: {
+        SET_LOADING(state: any, payload: boolean = false) {
+            state.isLoading = payload
+        },
+        SET_CHAPTER(state: any, payload: ChapterItem) {
+            state.chapter = payload
+        },
+        SET_BOOK(state: any, payload: BookItem) {
+            state.book = payload
+        },
+    },
+
+    actions: {
+        async fetch(
+            { commit }: any,
+            { bookId, chapterId }: { bookId?: string, chapterId?: string }
+        ) {
+            commit('SET_LOADING', true)
+
+            try {
+                const { app }: any = this
+                const responseBook = await app.$services.book.byId({
+                    id: Number(bookId)
+                })
+
+                const responseChapter = await app.$services.chapter.byId({
+                    id: Number(chapterId)
+                })
+
+                commit('SET_CHAPTER', responseChapter)
+                commit('SET_BOOK', responseBook)
+
+                commit('SET_LOADING')
+            } catch {
+                // do something
+
+                commit('SET_LOADING')
+            }
+        },
+    },
+}
